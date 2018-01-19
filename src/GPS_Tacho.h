e174fe1ac748fc43e5c0ee83cde3d17d4a997249
Colchicum <jonas@hoffestival.de>
2018-01-19 22:20:04 +0100

    init commit of src files

---
+++ src/GPS_Tacho.h
@@ -0,0 +1,22 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _GPS_Tacho_H_
+#define _GPS_Tacho_H_
+#include "Arduino.h"
+//add your includes for the project GPS_Tacho here
+
+
+//end of add your includes here
+
+
+//add your function definitions for the project GPS_Tacho here
+
+
+
+
+//Do not add code below this line
+#endif /* _GPS_Tacho_H_ */
